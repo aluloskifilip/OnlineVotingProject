9194bd3f3a50cfbfab2d2f4fec1e43752d99caf9
aluloskifilip <60985180+aluloskifilip@users.noreply.github.com>
2020-02-12 20:53:42 +0100

    Add files via upload

---
+++ client/build/precache-manifest.ea7f04aaed177b1fa8bc869e42b148d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55618c43a9c4b17b58c5697bd6cffff7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "928029f44cd9a1bd6e92",
+    "url": "/static/css/2.2cfb2263.chunk.css"
+  },
+  {
+    "revision": "cf2557c02aaa0ba744ac",
+    "url": "/static/css/main.c72c8951.chunk.css"
+  },
+  {
+    "revision": "928029f44cd9a1bd6e92",
+    "url": "/static/js/2.540ee7e8.chunk.js"
+  },
+  {
+    "revision": "cf2557c02aaa0ba744ac",
+    "url": "/static/js/main.25febcee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
